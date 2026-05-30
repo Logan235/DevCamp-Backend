f9376802f9d2b4917229f7c1f074217a34f9d209
{LUANNGUYEN} <{nguyenthanhluan13022006@gmail.com}>
2026-05-31 00:17:21 +0700

    Extract JWT from the Authorization header as a Bearer token in line 10

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -7,12 +7,13 @@ import { ConfigService } from '@nestjs/config';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extract JWT from the Authorization header as a Bearer token
       ignoreExpiration: false, // check expiration of JWT if false it will auto check
       secretOrKey: configService.get<string>('secretJwt') as string, //Read secret key from .env file
     });
   }
 
+  // This is method that will be called by Passport after it verifies the JWT token. The payload parameter contains the decoded JWT payload.
   validate(payload: Record<string, unknown>) {
     return payload;
   }
